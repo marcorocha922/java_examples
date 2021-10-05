de39cd313f8d735d97e3a5dad41328c01a62e1f9
marcorocha922 <64110507+marcorocha922@users.noreply.github.com>
2021-10-06 00:37:33 +0100

    teste format

--- Exercicios/media_notas.java
+++ Exercicios/media_notas.java
@@ -6,22 +6,22 @@ public class media_notas {
     public static void main(String[] args) {
 
         float[] notas = new float[5];
-        float media=0;
+        float media = 0;
         String nome;
 
         System.out.print("Qual o nome do aluno?: ");
 
         Scanner teclado = new Scanner(System.in);
         nome = teclado.nextLine();
-        
-        for( int i=0; i<5; i++){
+
+        for (int i = 0; i < 5; i++) {
             System.out.print("\nNota :");
-            notas[i]=teclado.nextFloat();
-            media+=notas[i];
+            notas[i] = teclado.nextFloat();
+            media += notas[i];
         }
 
         teclado.close();
-        media/=5;
+        media /= 5;
         System.out.printf("Média de notas do aluno %s é de %.1f valores.", nome, media);
     }
 }
